4e6ce3d790b26a7cee72f6b97a814fc3956a7003
t-jadang-msft <t-jadang@microsoft.com>
2020-08-06 10:42:05 -0700

    Added timer trigger, added GetCursor method

--- Changefeed/ChangefeedSample/ChangefeedSample.cs
+++ Changefeed/ChangefeedSample/ChangefeedSample.cs
@@ -13,6 +13,10 @@ namespace ChangeFeedSample
 {
     public static class ChangefeedSample
     {
+        //Example predicate to filter events from Changefeed
+        static string containerName = "test-changefeed-container";
+        static Predicate<BlobChangeFeedEvent> containerCheck = (BlobChangeFeedEvent changeFeedEvent) => { return changeFeedEvent.Subject.Contains("/containers/" + containerName + "/"); };
+
         /* This program outputs events from a storage account's changefeed, which may be filtered. 
          * The program iterates through the changefeed using a cursor, which will be saved in a container of the specified storage account. 
          * Before starting the program, delete the cursor from previous runs from the storage account, if it exists
@@ -20,41 +24,17 @@ namespace ChangeFeedSample
         [FunctionName("ChangeFeedSample")]
         public static void Run([TimerTrigger("0 */30 * * * *")]TimerInfo myTimer, ILogger log)
         {
-            string connectionString = "CONNECTION_STRING";
-
-            //Example predicate to filter events from Changefeed
-            string containerName = "test-changefeed-container";
-            Predicate<BlobChangeFeedEvent> containerCheck = (BlobChangeFeedEvent changeFeedEvent) => { return changeFeedEvent.Subject.Contains("/containers/" + containerName + "/"); };
+            string connectionString = "CONNECTION_STRING";          
                         
             BlobServiceClient blobServiceClient = new BlobServiceClient(connectionString);
             BlobChangeFeedClient changeFeedClient = blobServiceClient.GetChangeFeedClient();
             BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient("storingcursorcontainer");
             BlobClient blobClient = new BlobClient(connectionString, "cursorcontainer","storingcursorblob");                      
                       
-            string continuationToken = null;
-
-            if(blobClient.Exists())
-            {
-                //If the continuationToken exists in blob, download and use it
-                var stream = new MemoryStream();
-                blobClient.DownloadTo(stream);
-                continuationToken = Encoding.UTF8.GetString(stream.ToArray());
-            }            
-            else
-            {
-                //If the continuationToken does not exist in the blob, get the continuationToken from the first item
-                Page<BlobChangeFeedEvent> page = changeFeedClient.GetChanges().AsPages(pageSizeHint: 1).First<Page<BlobChangeFeedEvent>>();
-                BlobChangeFeedEvent changeFeedEvent = page.Values.First<BlobChangeFeedEvent>();
-
-                if (containerCheck.Invoke(changeFeedEvent))
-                {
-                    log.LogInformation($"event: {changeFeedEvent.EventType} at {changeFeedEvent.Subject.Replace("/blobServices/default", "")} on {changeFeedEvent.EventTime}");
-                }
-                continuationToken = page.ContinuationToken;                             
-            }      
+            string continuationToken = GetCursor(blobClient,changeFeedClient,log);                            
 
             //Loop through the events in Changefeed with the continuationToken
-            foreach (Page<BlobChangeFeedEvent> page in changeFeedClient.GetChanges(continuationToken: continuationToken).AsPages(pageSizeHint: 1))
+            foreach (Page<BlobChangeFeedEvent> page in changeFeedClient.GetChanges(continuationToken: continuationToken).AsPages())
             {
                     foreach (BlobChangeFeedEvent changeFeedEvent in page.Values)
                     {
@@ -69,6 +49,32 @@ namespace ChangeFeedSample
             //Upload new continuationToken to blob
             var upStream = new MemoryStream(Encoding.ASCII.GetBytes(continuationToken));
             blobClient.Upload(upStream, true);
+        }
+
+        private static string GetCursor(BlobClient blobClient, BlobChangeFeedClient changeFeedClient, ILogger log)
+        {
+            string continuationToken = null; 
+
+            if (blobClient.Exists())
+            {
+                //If the continuationToken exists in blob, download and use it
+                var stream = new MemoryStream();
+                blobClient.DownloadTo(stream);
+                continuationToken = Encoding.UTF8.GetString(stream.ToArray());
+            }
+            else
+            {
+                //If the continuationToken does not exist in the blob, get the continuationToken from the first item
+                Page<BlobChangeFeedEvent> page = changeFeedClient.GetChanges().AsPages(pageSizeHint: 1).First<Page<BlobChangeFeedEvent>>();
+                BlobChangeFeedEvent changeFeedEvent = page.Values.First<BlobChangeFeedEvent>();
+                if (containerCheck.Invoke(changeFeedEvent))
+                {
+                    log.LogInformation($"event: {changeFeedEvent.EventType} at {changeFeedEvent.Subject.Replace("/blobServices/default", "")} on {changeFeedEvent.EventTime}");
+                }                              
+                continuationToken = page.ContinuationToken;
+            }
+
+            return continuationToken;
         }
     }
 }
--- Changefeed/ExampleEventCreator/ExampleEventCreator.cs
+++ Changefeed/ExampleEventCreator/ExampleEventCreator.cs
@@ -3,26 +3,57 @@ using System;
 using System.IO;
 using System.Text;
 using System.Configuration;
+using System.Threading;
 
 namespace ExampleEventCreator
 {
     class ExampleEventCreator
     {
+        static int count;
+        static string connectionString = ConfigurationManager.AppSettings["connectionString"];
+        static string containerName = "test-changefeed-container";
+
         //Creates container and blobs within a storage account to populate changefeed and add example events
         static void Main(string[] args)
         {
-            string connectionString = ConfigurationManager.AppSettings["connectionString"]; ;
-            string containerName = "test-changefeed-container";
-            BlobServiceClient blobServiceClient = new BlobServiceClient(connectionString);
-            BlobContainerClient blobContainerClient = blobServiceClient.CreateBlobContainer(containerName);               
+            count = 0;
             
-            for(int i = 0; i < 5; i++)
+            BlobServiceClient blobServiceClient = new BlobServiceClient(connectionString);
+            BlobContainerClient blobContainerClient = blobServiceClient.CreateBlobContainer(containerName);
+
+            Timer timer = null;
+            Console.WriteLine("Press Space to start the timer. Press Enter to stop the timer.");
+            while (true)
             {
-                var upStream = new MemoryStream(Encoding.ASCII.GetBytes("blob"));
-                BlobClient blobClient = new BlobClient(connectionString, containerName, "exampleblob" + i.ToString());
-                blobClient.Upload(upStream,true);
+                if(Console.ReadKey().Key == ConsoleKey.Spacebar)
+                {
+                    Console.WriteLine("Starting timer...");
+                    timer = new Timer(new TimerCallback(CreateEvents), null, 0, 1800000);
+                }
+                else if(Console.ReadKey().Key == ConsoleKey.Enter)
+                {
+                    Console.WriteLine("Stopping timer...");
+                    timer.Change(
+                        Timeout.Infinite,
+                        Timeout.Infinite);
+                    break;
+                }            
             }
-            Console.WriteLine("Finished creating example events");
+
+                     
+        }
+
+        static void CreateEvents(Object stateinfo)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                var upStream = new MemoryStream(Encoding.ASCII.GetBytes("Lorem Ipsum"));
+                BlobClient blobClient = new BlobClient(connectionString, containerName, "exampleblob" + count.ToString());
+                blobClient.Upload(upStream, true);
+                count++;
+            }
+
+            Console.WriteLine("Created example events");
         }
     }
 }
