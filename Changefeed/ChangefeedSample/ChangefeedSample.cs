74bb1b9f9aa4aac17201dd93abc838ba9004c152
t-jadang-msft <t-jadang@microsoft.com>
2020-08-07 16:49:23 -0700

    Added Constants.cs file

--- Changefeed/ChangefeedSample/ChangefeedSample.cs
+++ Changefeed/ChangefeedSample/ChangefeedSample.cs
@@ -8,18 +8,21 @@ using Azure;
 using System.IO;
 using System.Text;
 using System.Linq;
+using ChangefeedSample;
 
 namespace ChangeFeedSample
 {
     public static class ChangefeedSample
-    {   
+    {
         // Storage account connection string
-        static string connectionString = "CONNECTION_STRING";
+        static string connectionString = Constants.connectionString;
 
-        // Example predicate to filter events from Changefeed
-        static string containerName = "test-changefeed-container";
-        static string eventType = "BlobCreated";
+        // Example predicates to filter events from Changefeed
+        static string containerName = Constants.containerName;
+        static string blobName = Constants.blobName;
+        static string eventType = Constants.eventType;
         static Predicate<BlobChangeFeedEvent> containerCheck = (BlobChangeFeedEvent changeFeedEvent) => { return changeFeedEvent.Subject.Contains("/containers/" + containerName + "/"); };
+        static Predicate<BlobChangeFeedEvent> blobCheck = (BlobChangeFeedEvent changeFeedEvent) => { return changeFeedEvent.Subject.Contains("/blobs/" + blobName); };
         static Predicate<BlobChangeFeedEvent> eventTypeCheck = (BlobChangeFeedEvent changeFeedEvent) => { return changeFeedEvent.EventType == eventType; };
 
 
@@ -27,11 +30,13 @@ namespace ChangeFeedSample
          * This program outputs events from a storage account's changefeed, which may be filtered. 
          * The program iterates through the changefeed using a cursor, which will be saved in a container of the specified storage account. 
          * Before starting the program, delete the cursor from previous runs from the storage account, if it exists
-         * To run without filtering, remove if statement in Run() and GetCursor()
+         * To run without filtering, remove if statement in Run and GetCursor
+         * To change trigger interval, change the CRON string in Run
          */
         [FunctionName("ChangeFeedSample")]
         public static void Run([TimerTrigger("0 */30 * * * *")]TimerInfo myTimer, ILogger log)
-        {           
+        {
+            // Create client for Changefeed
             BlobServiceClient blobServiceClient = new BlobServiceClient(connectionString);
             BlobChangeFeedClient changeFeedClient = blobServiceClient.GetChangeFeedClient();
             try
@@ -43,7 +48,8 @@ namespace ChangeFeedSample
                 BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient("cursorstoragecontainer");
             }
             BlobClient blobClient = new BlobClient(connectionString, "cursorstoragecontainer","cursorBlob");                      
-                      
+            
+            // Create and get cursor for Changefeed
             string continuationToken = GetCursor(blobClient,changeFeedClient,log);                            
 
             // Loop through the events in Changefeed with the continuationToken
@@ -51,7 +57,7 @@ namespace ChangeFeedSample
             {
                     foreach (BlobChangeFeedEvent changeFeedEvent in page.Values)
                     {
-                        if (containerCheck.Invoke(changeFeedEvent) && eventTypeCheck.Invoke(changeFeedEvent))
+                        if (containerCheck.Invoke(changeFeedEvent) && eventTypeCheck.Invoke(changeFeedEvent) && blobCheck.Invoke(changeFeedEvent))
                         {
                             log.LogInformation($"event: {changeFeedEvent.EventType} at {changeFeedEvent.Subject.Replace("/blobServices/default","")} on {changeFeedEvent.EventTime}");
                         }                    
@@ -64,6 +70,7 @@ namespace ChangeFeedSample
             blobClient.Upload(upStream, true);
         }
 
+        // Gets a cursor from container or creates a new cursor if it does not exist
         private static string GetCursor(BlobClient blobClient, BlobChangeFeedClient changeFeedClient, ILogger log)
         {
             string continuationToken = null; 
@@ -80,7 +87,7 @@ namespace ChangeFeedSample
                 // If the continuationToken does not exist in the blob, get the continuationToken from the first item
                 Page<BlobChangeFeedEvent> page = changeFeedClient.GetChanges().AsPages(pageSizeHint: 1).First<Page<BlobChangeFeedEvent>>();
                 BlobChangeFeedEvent changeFeedEvent = page.Values.First<BlobChangeFeedEvent>();
-                if (containerCheck.Invoke(changeFeedEvent) && eventTypeCheck.Invoke(changeFeedEvent))
+                if (containerCheck.Invoke(changeFeedEvent) && eventTypeCheck.Invoke(changeFeedEvent) && blobCheck.Invoke(changeFeedEvent))
                 {
                     log.LogInformation($"event: {changeFeedEvent.EventType} at {changeFeedEvent.Subject.Replace("/blobServices/default", "")} on {changeFeedEvent.EventTime}");
                 }                              
---
+++ Changefeed/ChangefeedSample/Constants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChangefeedSample
+{
+    public static class Constants
+    {
+        public const string connectionString = "CONNECTION_STRING";
+        public const string containerName = "test-changefeed-container";
+        public const string eventType = "BlobCreated";
+        public const string blobName = "exampleblob0";
+    }
+}
